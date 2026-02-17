7ab6ad9b71b310db09536d049c0abaf172e38c46
Zeynab Jalalian <zjalalianweb@gmail.com>
2026-02-17 17:51:05 +0330

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./App.scss";
 
 function App() {
-  return (
-    <div>
-      <div id="wrapper">
+  const [temp, setTemp] = useState(34);
 
-      </div>
-    </div>
-  )
+  function decrease() {
+    const newTemp = temp - 1;
+    setTemp(newTemp);
+  }
+
+  function increase() {
+    const newTemp = temp + 1;
+    setTemp(newTemp);
+  }
+
+  return (
+    <section
+      id="wrapper"
+      className={
+       temp < 15 ? "too-cold"
+       : temp< 25 ? "cold"
+       : temp<30 ? "warm"
+       : "too-warm"
+      }
+    >
+      <header></header>
+      <main id="main" className="">
+        <p id="temp">{temp}C</p>
+        <div id="buttons">
+          <button id="decrease" onClick={decrease}>
+            Decrease
+          </button>
+          <button id="increase" onClick={increase}>
+            Increase
+          </button>
+        </div>
+      </main>
+      <footer></footer>
+      <div id="bg-blur"></div>
+    </section>
+  );
 }
 
-export default App
+export default App;
